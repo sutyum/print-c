86c314d8b13133d94e30a5b086560dcb0b7ee952
Satyam Tiwary <satyam@technoculture.io>
2024-08-19 16:33:29 +0530

    feat: register custom types

--- examples/simple.c
+++ examples/simple.c
@@ -1,9 +1,33 @@
-#include "../print.h"
+#define PRINT_IMPLEMENTATION
+#include "print.h"
 #include <unistd.h>
 
 // Implement _putchar for your specific environment
 void _putchar(char c) {
     write(STDOUT_FILENO, &c, 1);
+}
+
+// Define custom structs
+typedef struct {
+    int x;
+    int y;
+} Point;
+
+typedef struct {
+    char name[20];
+    int age;
+} Person;
+
+// Custom print function for Point
+void print_point(const void* data) {
+    const Point* p = (const Point*)data;
+    print("({}, {})", p->x, p->y);
+}
+
+// Custom print function for Person
+void print_person(const void* data) {
+    const Person* person = (const Person*)data;
+    print("{s}, {d} years old", person->name, person->age);
 }
 
 int main() {
@@ -12,16 +36,24 @@ int main() {
     int number = 42;
     unsigned int hex_number = 0xABCD;
     
+    // Register custom types
+    register_custom_type("Point", print_point);
+    register_custom_type("Person", print_person);
+    
+    Point p = {10, 20};
+    Person person = {"Bob", 25};
+    
     print("Hello {s}, you are {f:.2} years old.\n", name, age);
     print("Decimal: {d}, Hexadecimal: {x}, Binary: {b}\n", number, hex_number, number);
     print("Character: {c}, String: {s}\n", 'A', "Hello, World!");
-    
-    // Test NULL string
     print("NULL string: {s}\n", (char*)NULL);
     
-    // Test pointer printing
     int* ptr = &number;
     print("Pointer to number: {p}\n", (void*)ptr);
+    
+    // Use custom types
+    print("Point: {:Point}\n", &p);
+    print("Person: {:Person}\n", &person);
     
     return 0;
 }
--- print.h
+++ print.h
@@ -3,13 +3,22 @@
 
 #include <stdarg.h>
 #include <stdint.h>
+#include <stddef.h>
+#include <string.h>
 
 #ifndef NULL
 #define NULL ((void*)0)
 #endif
 
+#define MAX_TYPE_NAME_LENGTH 32
+#define MAX_CUSTOM_TYPES 64
+
+// Function pointer type for custom print functions
+typedef void (*custom_print_fn)(const void* data);
+
 // Function prototypes
 void print(const char* format, ...);
+void register_custom_type(const char* type_name, custom_print_fn fn);
 
 // Internal helper functions
 static void print_char(char c);
@@ -24,12 +33,60 @@ static void print_double(double value, int precision);
 // These functions should be implemented by the user
 extern void _putchar(char c);
 
+#ifdef PRINT_IMPLEMENTATION
+
+static struct {
+    char type_name[MAX_TYPE_NAME_LENGTH];
+    custom_print_fn fn;
+} custom_types[MAX_CUSTOM_TYPES];
+
+static size_t num_custom_types = 0;
+
+void register_custom_type(const char* type_name, custom_print_fn fn) {
+    if (num_custom_types < MAX_CUSTOM_TYPES && strlen(type_name) < MAX_TYPE_NAME_LENGTH) {
+        strncpy(custom_types[num_custom_types].type_name, type_name, MAX_TYPE_NAME_LENGTH - 1);
+        custom_types[num_custom_types].type_name[MAX_TYPE_NAME_LENGTH - 1] = '\0';
+        custom_types[num_custom_types].fn = fn;
+        num_custom_types++;
+    }
+}
+
+static custom_print_fn get_custom_print_fn(const char* type_name) {
+    for (size_t i = 0; i < num_custom_types; i++) {
+        if (strcmp(custom_types[i].type_name, type_name) == 0) {
+            return custom_types[i].fn;
+        }
+    }
+    return NULL;
+}
+
 void print(const char* format, ...) {
     va_list args;
     va_start(args, format);
 
     while (*format) {
-        if (*format == '{' && *(format + 1) && *(format + 1) != '{') {
+        if (*format == '{' && *(format + 1) == ':') {
+            format += 2; // Skip '{:'
+            char type_name[MAX_TYPE_NAME_LENGTH] = {0};
+            size_t i = 0;
+            while (*format && *format != '}' && i < MAX_TYPE_NAME_LENGTH - 1) {
+                type_name[i++] = *format++;
+            }
+            if (*format == '}') {
+                custom_print_fn custom_fn = get_custom_print_fn(type_name);
+                if (custom_fn) {
+                    void* arg = va_arg(args, void*);
+                    custom_fn(arg);
+                } else {
+                    print_string("{:");
+                    print_string(type_name);
+                    print_char('}');
+                }
+            } else {
+                print_string("{:");
+                print_string(type_name);
+            }
+        } else if (*format == '{' && *(format + 1) && *(format + 1) != '{') {
             format++; // Skip '{'
             switch (*format) {
                 case '}': // Just '{}'
@@ -169,5 +226,7 @@ static void print_double(double value, int precision) {
         }
     }
 }
+
+#endif // PRINT_IMPLEMENTATION
 
 #endif // PRINT_H
